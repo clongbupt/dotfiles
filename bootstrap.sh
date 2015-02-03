de99c3f9c9d43f3e661426395afdb2f8f708bfd0
clongbupt <clongbupt@gmail.com>
2015-02-03 20:04:07 +0900

    add vim swap and undo directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,8 @@ function doIt() {
 	rsync -L --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
+        mkdir ~/.vim/swaps
+        mkdir ~/.vim/undo
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
